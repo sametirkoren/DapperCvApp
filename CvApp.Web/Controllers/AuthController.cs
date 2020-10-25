26b83ad198f2dd880f7e02812055fb0a59de3d37
Samet İrkören <sametirkoren@gmail.com>
2020-10-25 10:43:38 +0300

    Custom Cookie Based Auth

--- CvApp.Web/Controllers/AuthController.cs
+++ CvApp.Web/Controllers/AuthController.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using CvApp.Business.Interfaces;
 using CvApp.Web.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CvApp.Web.Controllers
@@ -24,13 +27,26 @@ namespace CvApp.Web.Controllers
 
         [HttpPost]
 
-        public IActionResult Login(AppUserLoginModel appUserLoginModel)
+        public async Task<IActionResult> Login(AppUserLoginModel appUserLoginModel)
         {
             if (ModelState.IsValid)
             {
                 if (_appUserService.CheckUser(appUserLoginModel.UserName, appUserLoginModel.Password))
                 {
-                    
+                    var claims = new List<Claim>
+                    {
+                        new Claim(ClaimTypes.Name,appUserLoginModel.UserName),
+                        new Claim(ClaimTypes.Role,"Admin"),
+                    };
+
+                    var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                    var authProperties = new AuthenticationProperties
+                    {
+                        IsPersistent    = appUserLoginModel.RememberMe
+                    };
+
+                    await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity), authProperties);
+                    return RedirectToAction("Index", "Home", new { @area = "Admin" });
                 }
 
                 ModelState.AddModelError("", "Kullanıcı adı veya parola hatalı");
